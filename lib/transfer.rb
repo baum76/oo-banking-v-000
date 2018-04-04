fecc9d0b23dceab03d55a95fb39fb95347e64828
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-04 23:02:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    end   
+    end
   end
 
   def reverse_transfer
@@ -28,6 +28,6 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "reversed"
-    end  
+    end
   end
 end
