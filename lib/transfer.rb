82d762afa7bbe4e239814595ec2f0f39b6f1596f
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-04 23:11:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,8 +27,8 @@ class Transfer
 
   def reverse_transfer
     if @status == "complete"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
+      @sender.balance += @amount
+      @receiver.balance -= @amount
       @status = "reversed"
       #binding.pry
 
