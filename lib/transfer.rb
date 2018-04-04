e38f58450640611f1c349a5ee4793b0f5691a895
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-04 23:00:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,4 +11,15 @@ class Transfer
   def valid?
     @sender.valid? && @receiver.valid? ? true : false
   end
+
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."  
+  end
+
 end
