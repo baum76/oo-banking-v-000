016bf75ed5af3c7cef317e10f1269d428edd3d14
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-04 22:38:46 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,8 @@
 class BankAccount
-attr_accessor :bank_account
+  attr_accessor :bank_account
 
-def initialize(bank_account)
-  @bank_account = bank_account
+  def initialize(bank_account)
+    @bank_account = bank_account
 
-end
+  end
 end
