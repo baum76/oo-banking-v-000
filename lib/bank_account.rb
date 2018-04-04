37f94359ef8659ee0374b3878c83d09c8f2eba16
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-04 22:42:51 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,14 @@
 class BankAccount
   attr_accessor :bank_account, :balance, :status
   attr_reader :name
-  
+
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
+  end
+
+  def deposit
 
   end
 end
